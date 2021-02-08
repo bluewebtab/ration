a8332f77f0d1ef0a4d9a5a533167605fe547b199
Ronny Gutierrez <ronny.icne@gmail.com>
2021-02-07 17:36:54 -0800

    Added a new route for application

--- config/custom.js
+++ config/custom.js
@@ -58,13 +58,13 @@ module.exports.custom = {
 
   // The sender that all outgoing emails will appear to come from.
   fromEmailAddress: "ronny.icne@gmail.com",
-  fromName: "The NEW_APP_NAME Team",
+  fromName: "The ration Team",
 
   // Email address for receiving support messages & other correspondences.
   // > If you're using the default privacy policy, this will be referenced
   // > as the contact email of your "data protection officer" for the purpose
   // > of compliance with regulations such as GDPR.
-  internalEmailAddress: "support+development@example.com",
+  internalEmailAddress: "ronny.icne@gmail.com",
 
   // Whether to require proof of email address ownership any time a new user
   // signs up, or when an existing user attempts to change their email address.
